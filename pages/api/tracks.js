69c35d1c667db1105cd7ccaf62ce46015f217322
Arthur030 <arthur.olivier30@gmail.com>
2021-07-26 10:32:39 +0200

    first commit

---
+++ pages/api/tracks.js
@@ -0,0 +1,47 @@
+// import image1 from "/images/dancin.jpg";
+// import image2 from "/images/davie504.jpg";
+// import image3 from "/images/ricardo.jpg";
+// import image4 from "/images/shooting-star.jpg";
+// import image5 from "/images/theUntold.jpg";
+// import audio1 from "/audio/dancin.mp3";
+// import audio2 from "/audio/davie504.mp3";
+// import audio3 from "/audio/ricardo.mp3";
+// import audio4 from "/audio/shooting-star.mp3";
+// import audio5 from "/audio/theUntold.mp3";
+
+
+
+
+const tracks = [
+    {
+    title: "Song 1",
+    artist: "Artist 1",
+    img: "/images/dancin.jpg",
+    audio: "/audio/dancin.mp3"
+    },
+    {
+    title: "Song 2",
+    artist: "Artist 2",
+    img: "/images/davie504.jpg",
+    audio: "/audio/davie504.mp3"
+    },
+    {
+    title: "Song 3",
+    artist: "Artist 3",
+    img: "/images/ricardo.jpg",
+    audio: "/audio/ricardo.mp3"
+    },
+    {
+    title: "Song 4",
+    artist: "Artist 4",
+    img: "/images/shooting-star.jpg",
+    audio: "/audio/shooting-star.mp3"
+    },
+    {
+    title: "The Untold",
+    artist: "BBC",
+    img: "/images/theUntold.jpg",
+    audio: "/audio/theUntold.mp3"
+    }
+]
+export default tracks;
---
+++ pages/index.js
@@ -0,0 +1,218 @@
+import React, { useState, useRef, useEffect } from 'react';
+import tracks from './api/tracks';
+import { FaPlay, FaPause, FaBackward, FaForward, FaVolumeMute, FaVolumeUp } from 'react-icons/fa'
+
+function App() {
+  //track Index of tracks from tracks.js
+  const [tracksIndex, setTracksIndex] = useState(0);
+  //track if isPlaying for Play or Pause button
+  const [isPlaying, setIsPlaying] = useState(false);
+  //total duration of audio
+  const [duration, setDuration] = useState(0);
+  //current time of audio
+  const [currentTime, setCurrentTime] = useState(0);
+  //toggle mute or unMute
+  const [isMuted, setIsMuted] = useState(true);
+  //tracks in tracks.json
+  const {title, artist, audio, img} = tracks[tracksIndex];
+  
+
+  const audioRef = useRef();
+  const progressBarRef = useRef();
+  //to have the player paused on first render
+  const isReady = useRef(false);
+  const volume = useRef();
+  
+  // everytime we change song
+  useEffect( () => {
+    pause();
+    console.log(tracks[tracksIndex].img)
+    // for first render of the page, makes the player on pause
+    if (isReady.current) {
+      //fix the error promise undeffined when skipping songs too fast
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then(() => {
+          play();
+          setIsPlaying(true);
+        })
+        .catch(error => {
+          console.log("error promise play()");
+        })
+      }
+    } else {
+      // once page has loaded once
+      isReady.current = true;
+    }
+  }, [tracksIndex]);
+
+  // update on the current time of audio
+  useEffect(() => {
+    console.log(currentTime, 'useEffect on currentTime');
+    progressBarRef.current.value = Math.floor(audioRef.current.currentTime);
+    progressBarRef.current.style.setProperty('--move-progressbar', `${progressBarRef.current.value / calculateTime(audioRef.current.duration) * 100}%`);
+    // setCurrentTime(progressBarRef.current.value);
+    // animationRef.current = requestAnimationFrame(progressBarRef);
+    if (currentTime >= audioRef.current.duration) {
+      next();
+    }
+  }, [currentTime])
+
+
+  // useEffect(() => {
+  //   toggleMute();
+  //   console.log(isMuted);
+  // }, [isMuted])
+
+
+  // set duration everytime the player has loaded metadata
+  const onLoadedMetadata = () => {
+    const seconds = Math.floor(audioRef.current.duration);
+    // console.log("onloaded metadata");
+    setDuration(seconds);
+    progressBarRef.current.max = seconds;
+  };
+
+  // const updateCurrentTime = () => {
+  //   setCurrentTime(progressBarRef.current.value);
+  // }
+
+  // const whilePlaying = () => {
+  //   // progressBarRef.current.value = Math.floor(audioRef.current.currentTime);
+  //   // updateCurrentTime();
+  //   // console.log("whileplaying", animationRef.current);
+  //   // animationRef.current = requestAnimationFrame(whilePlaying);
+  // }
+  
+  const togglePlayPause = () => {
+    const prevState = isPlaying;
+    setIsPlaying(!prevState);
+    if (!prevState) {
+      play();
+    } else {
+      pause();
+    }
+  };
+
+  const play = () => {
+    audioRef.current.play();
+  };
+
+  const pause = () => {
+    audioRef.current.pause();
+  };
+  
+  const changeAudioToProgressBar = () => {
+    audioRef.current.currentTime = progressBarRef.current.value;
+    setCurrentTime(progressBarRef.current.value);
+  }
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  }
+
+
+  const next = () => {
+    if (tracksIndex < tracks.length - 1) {
+      setTracksIndex(tracksIndex + 1);
+    } else {
+      setTracksIndex(0);
+    }
+  }
+
+  const prev = () => {
+    if (tracksIndex -1 < 0) {
+      setTracksIndex(tracks.length -1);
+    } else {
+      setTracksIndex(tracksIndex - 1);
+    }
+  }
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+    if (!isMuted) {
+      audioRef.current.volume = volume.current.value / 100;
+    } else {
+      audioRef.current.volume = 0;
+    }
+  };
+
+  const changeVolume = () => {
+    audioRef.current.volume = volume.current.value / 100;
+  }
+
+  return (
+    <div className="App">
+      <audio 
+      ref={audioRef} 
+      src={audio} 
+      preload="metadata" 
+      onLoadedMetadata={onLoadedMetadata}
+      onTimeUpdate={ () => setCurrentTime(audioRef.current.currentTime)}
+      >
+      </audio>
+      <div className="image-container">
+      <img
+      className="image" 
+      src={img} 
+      alt={`track art for ${title} by ${artist}`}
+      />
+      </div>
+      <div className="player-container">
+
+      <h5 className="author">{artist}</h5>
+      <h5 className="title">{title}</h5>
+      <div className="button-container">
+      <button onClick={prev}>
+        <FaBackward />
+      </button>
+      <button onClick={togglePlayPause}>
+      {isPlaying ? <FaPause /> : <FaPlay />}
+      </button>
+      <button onClick={next}>
+        <FaForward />
+      </button>
+
+      <div className="volume-container">
+      <button onClick={toggleMute}>
+        {isMuted ? <FaVolumeUp /> : <FaVolumeMute />}
+      </button>
+      <input 
+          type="range"
+          className="volume-bar"
+          min="0"
+          max="100"
+          defaultValue="50"
+          ref={volume}
+          onChange={changeVolume}
+          />
+      </div>
+
+      </div>
+      <div>
+      </div>
+      <input 
+        className="progress-bar"
+        type="range"
+        defaultValue="0"
+        ref={progressBarRef}
+        onChange={changeAudioToProgressBar}
+      />
+      <div className="time-container">
+        <h5>{calculateTime(currentTime)}</h5>
+        <h5>{ (duration && !isNaN(duration)) && calculateTime(duration) }</h5>
+      </div>
+
+      </div>
+      <h1>audio-player-clean</h1>
+    </div>
+
+  );
+}
+
+
+export default App;
